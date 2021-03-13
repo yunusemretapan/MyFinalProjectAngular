e090e83912f7d3d8f38420220626551590b23ead
yunusemretapan <37017446+yunusemretapan@users.noreply.github.com>
2021-03-13 23:52:14 +0300

    MyFinalProjectAngular

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -34,4 +34,12 @@ export class CategoryComponent implements OnInit {
     }
   }
 
+  getAllCategoryClass(){
+    if(!this.currentCategory){
+      return "list-group-item active"
+    }
+    else{
+      return "list-group-item"
+    }
+  }
 }
